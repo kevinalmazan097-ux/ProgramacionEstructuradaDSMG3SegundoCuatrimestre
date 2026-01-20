89a1a4aa41a9b69a38fc90ad5674a4933f801460
GarciaKA <kevinalmazan097@gmail.com>
2026-01-20 14:55:28 -0600

    Calificaciones.java y calificaciones.py: Agregado

---
+++ 02-EstructurasControl/programacionjava/Calificaciones.java
@@ -0,0 +1,25 @@
+
+import javax.swing.JOptionPane;
+
+public class Calificasiones {
+
+    public static void main(String[] args) {
+        //Declaracion de variables
+        double calif1 = 0.0, calif2 = 0.0, calif3 = 0.0;
+        double promedio = 0.0;
+
+        //Entrada de datos
+        calif1 = Double.parseDouble(JOptionPane.showInputDialog("introduce la calificacion"));
+        calif2 = Double.parseDouble(JOptionPane.showInputDialog("introduce la calificacion"));
+        calif3 = Double.parseDouble(JOptionPane.showInputDialog("introduce la calificacion"));
+
+        //operacion calcular promedio
+        promedio = (calif1 + calif2 + calif3) / 3.0;
+        //Estructura de seleccion simple
+        if (promedio >= 7.0) {
+            JOptionPane.showMessageDialog(null, "Aprovado");
+
+        }//cierra el if
+
+    }//Cierra main
+}//Cierra clase
---
+++ 02-EstructurasControl/programaspython/calificaciones.py
@@ -0,0 +1,14 @@
+
+# Entrada de datos
+calif_1=float(input("introduce la calificacion 1"))
+calif_2=float(input("introduce la calificacion 2"))
+calif_3=float(input("introduce la calificacion 3"))
+
+#Proceso Calcular Promedio
+
+promedio= (calif_1+calif_2+calif_3)//3
+
+#Estructura selectiva simple
+
+if promedio >= 7.0 :
+    print("Aprovado")
